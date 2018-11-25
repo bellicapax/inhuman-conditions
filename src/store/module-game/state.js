2b21a0d8ea28b0fdab9aa3e52ee0fc349a5419e3
Eris Koleszar <eris.koleszar@gmail.com>
2018-11-25 13:55:57 -0800

    Load JSON file to store

--- src/store/module-game/state.js
+++ src/store/module-game/state.js
@@ -1,3 +1,49 @@
-
-export default {
+function loadStore () {
+  var store = {}
+  var core = require('../../../data/inhumanCore.json')
+  store.notes = core.notes
+  store.penalties = core.penalties
+  store.packets = []
+  var starterPackets = require('../../../data/inhumanStarterPackets.json')
+  addPackets(store, starterPackets, core.roleTemplates)
+  // console.log(store)
+  return store
 }
+
+function addPackets (store, packetsObject, templates) {
+  packetsObject.packets.forEach((packet) => {
+    addPacket(store, packet, templates)
+  })
+}
+
+function addPacket (store, packet, templates) {
+  var filledPacket = {}
+  filledPacket.name = packet.name
+  filledPacket.inquiries = packet.inquiries
+  filledPacket.roles = []
+  packet.roles.forEach((role) => {
+    filledPacket.roles.push(fillRole(role, templates))
+  })
+  store.packets.push(filledPacket)
+}
+
+function fillRole (role, templates) {
+  var filledRole = role
+  var template = templates[role.name]
+  if (template) {
+    var directives = template['directives']
+    if (directives) {
+      if (!filledRole.directives) {
+        filledRole.directives = []
+      }
+      filledRole.directives.push.apply(filledRole.directives, directives)
+    }
+    var objective = template['objective']
+    if (objective) {
+      filledRole.objective = objective
+    }
+  }
+  return filledRole
+}
+
+export default loadStore()
