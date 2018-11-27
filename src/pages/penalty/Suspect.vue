95b0ae93f484813ae5bb8a4afd01db7d41453199
Eris Koleszar <eris.koleszar@gmail.com>
2018-11-27 00:01:41 -0800

    Add more formatting on Suspect Penalty selection

--- src/pages/penalty/Suspect.vue
+++ src/pages/penalty/Suspect.vue
@@ -1,9 +1,19 @@
 <template>
+  <div>
+  <h4>Selected</h4>
   <div class="row">
-    <div class="col-4" v-for="pen in getPenalties()" :key="pen">
-      <penalty :description="pen"/>
+    <div class="col-4" v-for="pen in selectedPenalties" :key="pen.description">
+      <penalty :description="pen.description" />
     </div>
   </div>
+  <hr>
+  <h4>Unselected</h4>
+  <div class="row">
+    <div class="col-4" v-for="pen in unselectedPenalties" :key="pen.description">
+      <penalty :description="pen.description" />
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -13,11 +23,31 @@ export default {
   components: {
     Penalty
   },
+  created () {
+    this.penalties = []
+    this.$store.state.penalties.forEach((pen) => {
+      this.penalties.push({
+        description: pen,
+        selected: false
+      })
+    })
+  },
   methods: {
-    selectPenalty (penalty) {
+    selectPenalty (penalty) {},
+    choosePenalty (penalty) {
+
+    }
+  },
+  computed: {
+    selectedPenalties () {
+      if (this.penalties) {
+        return this.penalties.filter((pen) => pen.selected)
+      }
     },
-    getPenalties () {
-      return this.$store.state.penalties
+    unselectedPenalties () {
+      if (this.penalties) {
+        return this.penalties.filter((pen) => !pen.selected)
+      }
     }
   }
 }
